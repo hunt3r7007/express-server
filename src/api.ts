ba1482edc703a821e63016d410aba634b98a0aba
OM GOHIL <93849677+hunt3r7007@users.noreply.github.com>
2023-06-30 12:00:33 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -11,7 +11,44 @@ app.use(express.text({ type: 'text/html' }));
 
 // Healthcheck endpoint
 app.get('/', (req, res) => {
-  res.status(200).send({ status: 'ok' });
+  res.status(200).send([
+    {
+      "id":1,
+      "title" :"ACRYLIC BOX" ,
+      "image": "E:/MyWebDevelopment/OM ACRYLIC ART/OM-Acrylic-Art/public/images/Image01.jpg",
+      "description":"These are jewellery Box. we can Box's make as per your size and order."
+    },
+    {
+      "id":2,
+      "title" :"ACRYLIC BOX" ,
+      "image": "E:/MyWebDevelopment/OM ACRYLIC ART/OM-Acrylic-Art/public/images/Image02.jpg",
+      "description":"These are jewellery Box. we can Box's make as per your size and order."
+    },
+    {
+      "id":3,
+      "title" :"ACRYLIC BOX" ,
+      "image": "E:/MyWebDevelopment/OM ACRYLIC ART/OM-Acrylic-Art/public/images/Image03.jpg",
+      "description":"These are jewellery Box. we can Box's make as per your size and order."
+    },
+    {
+      "id":4,
+      "title" :"ACRYLIC BOX" ,
+      "image": "E:/MyWebDevelopment/OM ACRYLIC ART/OM-Acrylic-Art/public/images/Image04.jpg",
+      "description":"These are jewellery Box. we can Box's make as per your size and order."
+    },
+    {
+      "id":5,
+      "title" :"ACRYLIC BOX" ,
+      "image": "E:/MyWebDevelopment/OM ACRYLIC ART/OM-Acrylic-Art/public/images/Image05.jpg",
+      "description":"These are jewellery Box. we can Box's make as per your size and order."
+    },
+    {
+      "id":6,
+      "title" :"ACRYLIC BOX" ,
+      "image": "E:/MyWebDevelopment/OM ACRYLIC ART/OM-Acrylic-Art/public/images/Image06.jpg",
+      "description":"These are jewellery Box. we can Box's make as per your size and order."
+    }
+  ]);
 });
 
 const api = express.Router();
